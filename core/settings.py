5908b94f7e1cb807afa4bb361834a5d83a6679cf
Viktor Zagoruyko <vzagoruyko@rbc.ru>
2025-10-01 11:18:48 +0500

    Ignore crawler errors in rollbar

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,5 @@
 import os
+import re
 import environ
 from pathlib import Path
 from celery.schedules import crontab
@@ -150,6 +151,7 @@ ROLLBAR = {
     "environment": "development" if DEBUG else "production",
     "code_version": "1.0",
     "root": BASE_DIR,
+    'ignorable_404_urls': (re.compile(r'.*\.(php|asp|aspx|cgi|bak|old|sql|tar|gz)$'),,),
 }
 
 LANGUAGE_CODE = "ru-ru"
