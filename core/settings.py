e30c9d753903aba4a837728d6434ebbfae7fafe1
Viktor Zagoruyko <vzagoruyko@rbc.ru>
2025-10-02 13:17:15 +0500

    Change cors settings

--- core/settings.py
+++ core/settings.py
@@ -98,9 +98,8 @@ if DEBUG:
     CORS_ALLOW_ALL_ORIGINS = True
 else:
     STATIC_ROOT = BASE_DIR / "staticfiles"
-    CORS_ALLOW_ALL_ORIGINS = True
     CORS_ALLOWED_ORIGINS = [
-        "*",
+        "http://localhost:5173",
     ]
     CORS_ALLOW_CREDENTIALS = True
     CORS_ALLOW_HEADERS = [
